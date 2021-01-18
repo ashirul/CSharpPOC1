f6941f0ceca5bbbadadf5f7a4a905e16fbcc355a
ashirul <ashirulansari@gmail.com>
2021-01-18 19:36:16 +0530

    Pushing the project

---
+++ CSharpPOC1/CSharpPOC1QS.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPOC1
+{
+    public class CSharpPOC1QS
+    {
+        // Fibonacci Series
+        public string fibTillFifty()
+        {
+            var fib = new List<long>();
+            long a = 0, b = 1, c;
+            fib.Add(a);
+            fib.Add(b);
+
+            for (var i = 1; i <= 48; i++)
+            {
+                c = a + b;
+                fib.Add(c);
+                a = b;
+                b = c;
+            }
+            return string.Join(",", fib);
+        }
+
+        // Armstrong Number
+        public void armstrong(int input)
+        {
+            var temp = input;
+            var inputLength = input.ToString().Length;
+            int rem, sum = 0;
+            while (temp > 0)
+            {
+                int pow = 1;
+                rem = temp % 10;
+                for (var i = 0; i < inputLength; i++)
+                    pow = rem * pow;
+                sum = sum + pow;
+                temp = temp / 10;
+            }
+            if (sum == input)
+                Console.WriteLine("Armstrong");
+            else
+                Console.WriteLine("Not Armstrong");
+        }
+
+        // Decimal to Binary
+        public string binary(string input)
+        {
+            var binBit = new List<long>();
+            var temp = Convert.ToInt64(input);
+            if (temp != 0)
+            {
+                while (temp > 0)
+                {
+                    binBit.Add(temp % 2);
+                    temp = temp / 2;
+                }
+                binBit.Reverse();
+                return string.Join("", binBit);
+            }
+            else if (temp == 0)
+                return "0";
+            return "";
+        }
+
+        // Consecutive 1's in Binary Equivalent
+        public void consecutiveones(string bin)
+        {
+            var tempBin = binary(bin);
+            var onlyOnes = tempBin.Split('0');
+            var count = 0;
+            if (bin == "0")
+            {
+                Console.WriteLine("Binary Equivalent: 0");
+                Console.WriteLine("Maximum number of Consecutive 1's: 0");
+            }
+            else
+            {
+                Array.Sort(onlyOnes);
+                count = Convert.ToInt32(onlyOnes[onlyOnes.Length - 1].Length);
+                Console.WriteLine();
+                if (count == 0)
+                    count = 1;
+                Console.WriteLine("Binary Equivalent: " + tempBin);
+                Console.WriteLine("Maximum number of Consecutive 1's: " + count);
+            }
+        }
+
+        // Sorting Array in Ascending Order
+        public void ascending(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // Sorting Array in Descending Order
+        public void descending(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // Swapping Values using a Third Variable
+        public void swapWithAThird(string first, string second)
+        {
+            string third = second;
+            second = first;
+            first = third;
+            Console.WriteLine("\nSwapping using a Third Variable.");
+            Console.WriteLine(String.Format("\nInput 1: {0}\nInput 2: {1}\n", first, second));
+        }
+
+        // Swapping Values without using a Third Variable
+        public void swapWithoutAThird(string first, string second)
+        {
+            first = first + " " + second;
+            for (var input = 0; input < first.Length; input++)
+            {
+                if (first[input] == ' ')
+                {
+                    second = first.Substring(0, input);
+                    first = first.Substring(input).Trim();
+                }
+            }
+            Console.WriteLine("Swapping Without using a Third Variable");
+            Console.WriteLine(String.Format("\nInput 1: {0}\nInput 2: {1}\n", first, second));
+        }
+
+        // Palindrome of String
+        public void palindrome(string input)
+        {
+            var inputLower = input.ToLower();
+            var inputLength = input.Length;
+            var temp = "";
+            for (var i = inputLength - 1; i >= 0; i--)
+            {
+                temp = temp + inputLower[i];
+            }
+            if (temp == inputLower)
+                Console.WriteLine("Palindrome");
+            else
+                Console.WriteLine("Not Palindrome");
+        }
+
+        // To Print ASCII of Upper Case Letters (A to Z)
+        public void upperCaseAscii()
+        {
+            string alphabetArray = "abcdefghijklmnopqrstuvwxyz";
+            var counter = 0;
+            foreach (var letter in alphabetArray.ToUpper())
+            {
+                Console.Write(String.Format("{0}: {1}   ", letter, (int)letter));
+                counter++;
+                if (counter % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("\n");
+        }
+
+        // To Print ASCII of Lower Case Letters (a to b)
+        public void lowerCaseAscii()
+        {
+            string alphabetArray = "abcdefghijklmnopqrstuvwxyz";
+            var counter = 0;
+            foreach (var letter in alphabetArray)
+            {
+                counter++;
+                {
+                    if (counter == 2)
+                        Console.Write(String.Format(" {0}: {1}   ", letter, (int)letter));
+                    else if (counter == 3)
+                        Console.Write(String.Format(" {0}: {1}   ", letter, (int)letter));
+                    else if (counter == 4)
+                        Console.Write(String.Format(" {0}: {1}   ", letter, (int)letter));
+                    else
+                        Console.Write(String.Format("{0}: {1}   ", letter, (int)letter));
+                }
+                if (counter % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ CSharpPOC1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPOC1
+{
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            CSharpPOC1QS obj = new CSharpPOC1QS();
+            Console.WriteLine("List of Solutions\n");
+            Console.WriteLine("1. Fibonacci Series till 50.\n2. Consecutive 1's in Binary Equivalent.\n3. Sorted Array\n4. Reverse of String\n5. Swapping Values\n6. Palindrome\n7. ASCII value of all Alphabets\n8. Armstrong Number ");
+            var flag = false;
+            do
+            {
+                Console.WriteLine("\nPlease Select the Solution You want to see\n");
+                Console.Write("Solution Number: ");
+                var solution = Convert.ToInt32(Console.ReadLine());
+                switch (solution)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(obj.fibTillFifty());
+                            flag = true;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("\nEnter a Number: ");
+                            var input = Console.ReadLine();
+                            obj.consecutiveones(input);
+                            flag = true;
+                            break;
+                        }
+                    case 3:
+                        {
+                            var numArray = new int[] { 5, 4, 3, 2, 1, 6, 7, 8, 9, 10 };
+
+                            Console.WriteLine("\nThe Numbers in the Array");
+                            Console.WriteLine(string.Join(",", numArray));
+                            Console.WriteLine();
+
+                            Console.WriteLine("The Array in Ascending Order");
+                            obj.ascending(numArray);
+                            Console.WriteLine(string.Join(",", numArray));
+                            Console.WriteLine();
+
+                            Console.WriteLine("The Array in Descending Order");
+                            obj.descending(numArray);
+                            Console.WriteLine(string.Join(",", numArray));
+                            Console.WriteLine();
+                            flag = true;
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("\nEnter the String");
+                            Console.Write("String: ");
+                            var input = Console.ReadLine();
+
+                            var charList = input.ToCharArray();
+                            var stringList = new List<string>(input.Split(' '));
+
+                            Array.Reverse(charList);
+
+                            stringList.Reverse();
+
+                            Console.WriteLine($"\nReversed String Word by Word\n{string.Join(" ", stringList)}");
+
+                            Console.WriteLine("\nReversed String Letter by Letter");
+                            Console.WriteLine(charList);
+
+                            Console.WriteLine();
+                            flag = true;
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("\nGive Two Inputs\n");
+                            Console.Write("Input 1: ");
+                            var input1 = Console.ReadLine();
+                            Console.Write("Input 2: ");
+                            var input2 = Console.ReadLine();
+                            obj.swapWithAThird(input1, input2);
+                            obj.swapWithoutAThird(input1, input2);
+                            flag = true;
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("\nEnter String");
+                            Console.Write("\nInput: ");
+                            var input = Console.ReadLine();
+                            obj.palindrome(input);
+                            flag = true;
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.WriteLine("\nUpper Case Alphabets with ASCII\n");
+                            obj.upperCaseAscii();
+                            Console.WriteLine("Lower Case Alphabets with ASCII\n");
+                            obj.lowerCaseAscii();
+                            flag = true;
+                            break;
+                        }
+                    case 8:
+                        {
+                            Console.Write("Enter a number:");
+                            var input = Convert.ToInt32(Console.ReadLine());
+                            obj.armstrong(input);
+                            flag = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("\nError: Invalid Input please try again...");
+                            flag = false;
+                            break;
+                        }
+                }
+                if(flag == true)
+                {
+                    while (true)
+                    {
+                        Console.Write("\nDo you want to see another solution(y/n): ");
+                        var neworexit = Console.ReadLine();
+                        if (neworexit == "y" || neworexit == "Y")
+                        {
+                            flag = false;
+                            break;
+                        }
+                        else if (neworexit == "n" || neworexit == "N")
+                        {
+                            flag = true;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nError: Wrong Input please try again ");
+                            continue;
+                        }
+                    }
+                    
+                }
+            } while (!flag);
+        }
+    }
+}
